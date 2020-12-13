503b935d6946ea3611300d4ad47f71beb3bfc597
usamit02 <usamit02@gmail.com>
2020-12-13 11:04:53 +0900

    book calculate fix

--- src/app/top/page/book/book.page.ts
+++ src/app/top/page/book/book.page.ts
@@ -18,26 +18,61 @@ import { User } from '../../../class';
 export class BookPage implements OnInit, OnDestroy {
   user: User;
   book: Book;
+  day:any;
+  days: Array<DayConfig>;
   private onDestroy$ = new Subject();
   constructor(private route: ActivatedRoute, private location: Location, private modal: ModalController, private ui: UiService,
     private userService: UserService, private api: ApiService,) { }
-
   ngOnInit() {
-    this.route.params.pipe(takeUntil(this.onDestroy$)).subscribe(async params => {
-      const res = await this.api.get('query', { select: ['*'], table: 'stay', where: { stay: params.id } });
-      if (res.stays.length) {
-        const stay = res.stays[0];
-        const amount=await this.calculate(stay.from,stay.to,params.id,stay.home,stay.price,stay.num);
-        this.userService.$.pipe(takeUntil(this.onDestroy$)).subscribe(user => {
-          this.user = user;          
-          this.book = { stay: params.id, home: stay.home, user: user.id, na: user.na, avatar: user.avatar, amount: amount, from: new Date(params.from), to: new Date(params.to) };
-        });
-      } else {
-        this.ui.alert('宿泊データがありません。');
-      }
+    this.route.params.pipe(takeUntil(this.onDestroy$)).subscribe(params => {
+      this.api.get('query', { select: ['*'], table: 'stay', where: { stay: params.id } }).then(async res => {
+        if (res.stays.length === 1) {
+          const stay = res.stays[0];          
+          const now = new Date();
+          const upper=new Date(now.setMonth(now.getMonth() + 1))
+          const where = { dated: { lower: this.dateFormat(now), upper: this.dateFormat(upper) } };
+          for(let d=now;d<=upper;d.setDate(d.getDate()+1)){
+            this.day[this.dateFormat(d)]={price:stay.price,num:stay.num,book:0,state:""}
+          }          
+          const home = await this.api.get('query', { select: ['*'], table: 'calendar', where: { ...where, home: stay.home } });
+          for (let calendar of home.calendars) {
+            if (calendar.rate) {
+              this.day[calendar.dated].price = stay.price * calendar.rate;
+            }
+          }
+          const calendar = await this.api.get('query', { select: ['*'], table: 'stay_calendar', where: { ...where, id: stay.id } });
+          for (let cal of calendar.stay_calendars) {
+            if (cal.close) {
+              this.day[cal.dated].status = 'close';
+            }
+            if (cal.num) {
+              this.day[cal.dated].num = cal.num;
+            }
+            if (cal.price) {
+              this.day[cal.dated].price = cal.price;
+            } else if (cal.rate) {
+              this.day[cal.dated].price = stay.price * cal.rate;
+            }
+          }
+          const book = await this.api.get('query', { select: ['*'], table: 'book', where: { ...where, stay: stay.id } });
+          for (let b of book.books) {
+            this.day[b.dated].book++;// = this.day[b.dated].book ? this.day[b.dated].book + 1 : 1;            
+            if (this.day[b.dated] > this.day[b.dated].num) { this.day[b.dated].state = 'full'; }
+          } 
+          //const amount=await this.calculate(stay.from,stay.to,params.id,stay.home,stay.price,stay.num);
+          this.userService.$.pipe(takeUntil(this.onDestroy$)).subscribe(user => {
+            this.user = user;
+            this.book = { stay: params.id, home: stay.home, user: user.id, na: user.na, avatar: user.avatar, amount: amount, from: new Date(params.from), to: new Date(params.to) };
+          });
+        } else {
+          this.ui.alert('宿泊データがありません。');
+        }
+      }).catch(err => {
+        this.ui.alert(`宿泊データの読み込みに失敗しました。\r\n${err.message}`);
+      })
     });
   }
-  calculate(from: string, to: string, id: number, home: number, price: number, num: number): Promise<number> {    
+  calculate(from: string, to: string, id: number, home: number, price: number, num: number): Promise<number> {
     const where = { dated: { lower: from, upper: to } };
     return new Promise((resolve, reject) => {
       this.api.get('query', { select: ['*'], table: 'calendar', where: { ...where, home: home } }).then(async res => {
@@ -115,6 +150,12 @@ export class BookPage implements OnInit, OnDestroy {
     this.ui.pop(`${this.book.na}を予約しました。`);
     this.location.back();
   }
+  dateFormat(date = new Date()) {//MySQL用日付文字列作成'yyyy-M-d H:m:s'    
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    var d = date.getDate();
+    return y + "-" + m + "-" + d;//+ " " + h + ":" + min + ":" + sec;
+  }
   ngOnDestroy() {
     this.onDestroy$.next();
   }
@@ -131,4 +172,10 @@ interface Option {
   id: number;
   na: string;
   price: number;
+}
+interface Day {
+  price: number;
+  num:number;
+  book:number;
+  state: string;
 }
